0772b29b4d46de8773fd3c8a11b324c0fbf0e798
diazroger0227 <diaz.roger0227@gmail.com>
2025-04-08 17:20:51 +0800

    xiugai

---
+++ DE/lander 1-修改中/main_script.js
@@ -0,0 +1,37 @@
+var lastSpin = false;
+var spinning = false;
+
+$(document).ready(function () {
+  function firstSpinCompleted() {
+    spinning = false;
+    lastSpin = true;
+    $('.popup').removeClass('active');
+    $('.popup.first-spin').addClass('active');
+    document.getElementById("fanfare1").play();
+  };
+
+  function lastSpinCompleted() {
+    spinning = false;
+    lastSpin = true;
+    $('.popup').removeClass('active');
+    $('.popup.last-spin').addClass('active');
+    document.getElementById("fanfare2").play();
+  };
+
+  $('.button').on('click', function (e) {
+    if (!spinning) {
+      document.getElementById("wheel").play();
+      if (!lastSpin) {
+        $('.wheel__wheel').addClass('first-spin');
+        spinning = true;
+        setTimeout(firstSpinCompleted, 5000);
+      } else {
+        $('.popup').removeClass('active');
+        $('.wheel__wheel').removeClass('first-spin');
+        $('.wheel__wheel').addClass('last-spin');
+        spinning = true;
+        setTimeout(lastSpinCompleted, 5000);
+      }
+    }
+  });
+});
